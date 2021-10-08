e4ae5a4f4349f8c6201f5fd755005e7a8fc828f0
Alex Sneddon <asneddon.dev@gmail.com>
2021-10-08 14:46:33 +1100

    Test data on gadi.

--- train.py
+++ train.py
@@ -10,14 +10,17 @@ class SignalDataset(Dataset):
         c_x = torch.load(coding_file).to(device)
         n_x = torch.load(noncoding_file).to(device)
 
+        print(f"Shape of coding tensor: \t{c_x.shape}")
+        print(f"Shape of noncoding tensor: \t{n_x.shape}")
+
         c_y = torch.zeros(c_x.shape[0], device=device)
         n_y = torch.ones(n_x.shape[0], device=device)
 
         self.data  = torch.cat((c_x, n_x))
         self.label = torch.cat((c_y, n_y))
 
-        print(self.data.shape)
-        print(self.label.shape)
+        print(f"Shape of total dataset: {self.data.shape}")
+        print(f"Shape of total labels: {self.label.shape}")
 
     def __len__(self):
         return len(self.label)
@@ -43,24 +46,28 @@ def main():
 
     # Create dataset
 
-    coding_file = "data.pt"
-    noncoding_file = "data.pt"
+    data_dir = "/g/data/xc17/Eyras/alex/working/rna-classifier/5_MakeDataset"
+    train_cfile = f"{data_dir}/train_coding.pt"
+    train_nfile = f"{data_dir}/train_noncoding.pt"
+    test_cfile = f"{data_dir}/test_coding.pt"
+    test_nfile = f"{data_dir}/test_noncoding.pt"
 
-    train_data = SignalDataset(coding_file, noncoding_file, device)
+    train_data = SignalDataset(train_cfile, train_nfile, device)
+    test_data = SignalDataset(test_cfile, test_nfile, device)
 
     # Create data loaders
 
-    train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+    # train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
 
-    for x, y in train_loader:
-        print(f"Shape of x: {x.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
+    # for x, y in train_loader:
+    #     print(f"Shape of x: {x.shape}")
+    #     print(f"Shape of y: {y.shape} {y.dtype}")
 
-        x = x.to('cpu')
-        x_d = x[0]
+    #     x = x.to('cpu')
+    #     x_d = x[0]
 
-        plt.plot(x_d)
-        plt.show()
+    #     plt.plot(x_d)
+    #     plt.show()
 
 
 
