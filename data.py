cb525c019958db0a83860f388544658d3678e7f1
Alex Sneddon <asneddon.dev@gmail.com>
2021-10-25 15:03:57 +1100

    Swap coding label to 1.

--- data.py
+++ data.py
@@ -3,17 +3,17 @@ from torch.utils.data.dataset import Dataset
 
 class SignalDataset(Dataset):
     def __init__(self, coding_file, noncoding_file):
-        c_x = torch.load(coding_file)
         n_x = torch.load(noncoding_file)
+        c_x = torch.load(coding_file)
 
-        print(f"Shape of coding tensor: \t{c_x.shape}")
         print(f"Shape of noncoding tensor: \t{n_x.shape}")
+        print(f"Shape of coding tensor: \t{c_x.shape}")
 
-        c_y = torch.zeros(c_x.shape[0], dtype=torch.long)
-        n_y = torch.ones(n_x.shape[0], dtype=torch.long)
+        n_y = torch.zeros(n_x.shape[0], dtype=torch.long)
+        c_y = torch.ones(c_x.shape[0], dtype=torch.long)
 
-        self.data  = torch.cat((c_x, n_x))
-        self.label = torch.cat((c_y, n_y))
+        self.data  = torch.cat((n_x, c_x))
+        self.label = torch.cat((n_y, c_y))
 
         print(f"Shape of total dataset: {self.data.shape}")
         print(f"Shape of total labels: {self.label.shape}")
