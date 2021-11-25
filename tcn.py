115d80db2dc8cb2657b2cf44ce906bebe91a393d
Alex Sneddon <asneddon.dev@gmail.com>
2021-11-25 11:56:19 +1100

    Refactor TCN.

--- tcn.py
+++ tcn.py
@@ -13,35 +13,37 @@ class Chomp1d(nn.Module):
         return x[:, :, :-self.chomp_size].contiguous()
 
 
+# TODO: Move inside class
+def conv_block(in_chan, out_chan, kernel_size, stride, dilation, padding, dropout):
+    layers = [
+        weight_norm(nn.Conv1d(in_chan, out_chan, kernel_size, stride=stride, padding=padding, dilation=dilation)),
+        Chomp1d(padding),
+        nn.ReLU(),
+        nn.Dropout(dropout)
+    ]
+    return nn.Sequential(*layers)
+
+
 class TemporalBlock(nn.Module):
-    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+    def __init__(self, in_chan, out_chan, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
-        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
-                                           stride=stride, padding=padding, dilation=dilation))
-        self.chomp1 = Chomp1d(padding)
-        self.relu1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(dropout)
 
-        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
-                                           stride=stride, padding=padding, dilation=dilation))
-        self.chomp2 = Chomp1d(padding)
-        self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(dropout)
+        # Causal convolutional blocks
+        self.blocks = nn.Sequential(
+            conv_block(in_chan, out_chan, kernel_size, stride, dilation, padding, dropout),
+            conv_block(out_chan, out_chan, kernel_size, stride, dilation, padding, dropout),
+        )
+        
+        self.downsample = nn.Conv1d(in_chan, out_chan, 1) if in_chan != out_chan else None
+        self.relu = nn.ReLU() # TODO: Rename activation
 
-        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
-                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
-        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
-        self.relu = nn.ReLU()
-        self.init_weights()
-
-    def init_weights(self):
-        self.conv1.weight.data.normal_(0, 0.01)
-        self.conv2.weight.data.normal_(0, 0.01)
-        if self.downsample is not None:
-            self.downsample.weight.data.normal_(0, 0.01)
+        # Initialise weights and biases
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.normal_(m.weight, mean=0, std=0.01)
 
     def forward(self, x):
-        out = self.net(x)
+        out = self.blocks(x)
         res = x if self.downsample is None else self.downsample(x)
         return self.relu(out + res)
 
