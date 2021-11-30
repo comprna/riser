a3f17021ded9f57c8c24b9c9a934cf50728f9626
Alex Sneddon <asneddon.dev@gmail.com>
2021-12-01 09:53:34 +1100

    CNN classifier.

--- cnn.py
+++ cnn.py
@@ -1,5 +1,4 @@
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
 from torchinfo import summary
 
 from utilities import get_config
@@ -19,34 +18,41 @@ class ConvNet(nn.Module):
         self.conv_layers = nn.ModuleList(layers)
 
         # Classifier
-        
+        if c.classifier == 'fc':
+            self.classifier = nn.Sequential(
+                nn.Flatten(1),
+                nn.Linear(37654, 4096), # TODO: Hardcoded
+                nn.Linear(4096, c.n_classes)
+            )
+        elif c.classifier == 'gap_fc':
+            self.classifier = nn.Sequential(
+                nn.AdaptiveAvgPool1d(1),
+                nn.Flatten(1),
+                nn.Linear(c.layer_channels[-1], c.n_classes)
+            )
+        elif c.classifier == 'gap':
+            self.classifier = nn.Sequential(
+                nn.Conv1d(c.layer_channels[-1], c.n_classes, 1),
+                nn.AdaptiveAvgPool1d(1)
+            )
+        else:
+            print("Typo in config file: Classifier = {c.classifier}")
 
-        self.linear1 = nn.Linear()
-        self.linear2 = nn.Linear()
-        self.linear3 = nn.Linear()
-
-        self.activation = nn.ReLU(inplace=True)
-        
     def forward(self, x):
         for layer in self.conv_layers:
             x = layer(x)
 
-        x = self.linear1(x)
-        x = self.activation(x)
-
-        x = self.linear2(x)
-        x = self.activation(x)
-
-        x = self.linear3(x)
+        x = self.classifier(x)
 
         return x
 
     def _make_layer(self, in_channels, out_channels, kernel_size, last=False):
         layers = [
-            nn.Conv1D(in_channels, out_channels, kernel_size),
-            nn.MaxPool1D(kernel_size=2, stride=2)
+            nn.Conv1d(in_channels, out_channels, kernel_size),
+            nn.MaxPool1d(kernel_size=2, stride=2)
         ]
 
+        # Activate all but the last hidden layer
         if last == False:
             layers.append(nn.ReLU(inplace=True))
         
@@ -54,13 +60,10 @@ class ConvNet(nn.Module):
 
 
 def main():
-    config = get_config('config.yaml').cnn
-
-    model = ConvNet(config)
-
+    config = get_config('config.yaml')
+    model = ConvNet(config.cnn)
     summary(model, input_size=(64, 1, 9036)) # (batch_size, dimension, seq_length)
 
 
 if __name__ == "__main__":
     main()
-    
