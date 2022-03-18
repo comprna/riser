2ff587f7af57a99c74ca11706d147c9f4d392f5b
Alex Sneddon <asneddon.dev@gmail.com>
2022-03-18 13:57:28 +1100

    Extract Client class.

---
+++ client.py
@@ -0,0 +1,52 @@
+import time
+
+import numpy as np
+
+from read_until import ReadUntilClient
+from read_until.read_cache import AccumulatingCache
+
+"""
+Advantages: https://softwareengineering.stackexchange.com/questions/298145/wrapping-third-party-library-is-best-practice#:~:text=In%20fact%2C%20wrapping%20third%2Dparty,are%20testing%20your%20own%20code.
+"""
+
+N_CHANNELS = 512
+
+class Client():
+    def __init__(self, logger):
+        self.logger = logger
+        
+        self.ru_client = ReadUntilClient(filter_strands=True, #TODO: Is this needed?
+                                         one_chunk=False,
+                                         cache_type=AccumulatingCache)
+
+    def start_streaming_reads(self):
+        self.ru_client.run(first_channel=1, last_channel=N_CHANNELS)
+        while self.ru_client.is_running is False:
+            time.sleep(0.1)
+            self.logger.info('Waiting for client to start streaming live reads.')
+        self.logger.info('Client is running.')
+    
+    def send_message_to_minknow(self, severity, message):
+        """
+        severity: Severity enum (value sent to API)
+        """
+        self.ru_client.connection.log.send_user_message(user_message=message,
+                                                severity=severity.value)
+    
+    def is_running(self):
+        return self.ru_client.is_running
+    
+    def get_read_chunks(self): # TODO: Rename
+        return self.ru_client.get_read_chunks(batch_size=N_CHANNELS, last=True)
+    
+    def get_raw_signal(self, read):
+        return np.frombuffer(read.raw_data, self.ru_client.signal_dtype)
+    
+    def reject_reads(self, reads, unblock_duration):
+        if reads: # TODO: What if this is omitted?
+            self.ru_client.unblock_read_batch(reads,
+                                              duration=unblock_duration)
+
+    def track_assessed_reads(self, reads):
+        if reads: # TODO: What if this is omitted?
+            self.ru_client.stop_receiving_batch(reads)
--- riser.py
+++ riser.py
@@ -7,10 +7,9 @@ from timeit import default_timer as timer
 from concurrent.futures import ThreadPoolExecutor
 from matplotlib import pyplot as plt
 import numpy as np
-from read_until import ReadUntilClient
-from read_until.read_cache import AccumulatingCache
 import torch
 
+from client import Client
 from model import Model
 from utilities import get_config
 from preprocess import SignalProcessor
@@ -39,33 +38,23 @@ class Severity(Enum):
     ERROR = 3
 
 
-def send_message_to_minknow(client, severity, message):
-    """
-    severity: Severity enum (value sent to API)
-    """
-    client.connection.log.send_user_message(user_message=message,
-                                            severity=severity.value)
+def analysis(client, model, processor, target, logger, duration=0.1, throttle=4.0):
+    client.send_message_to_minknow(
+        Severity.WARNING,
+        ('RISER will accept reads that are %s and reject all others. This will '
+        'affect the sequencing run.' % (target.name.lower())))
 
-
-def analysis(client, model, processor, target, logger, duration=0.1, throttle=4.0, batch_size=512):
-    send_message_to_minknow(client,
-                            Severity.WARNING,
-                            ('RISER will accept reads that are %s and reject '
-                            'all others. This will affect the sequencing run.' %
-                            (target.name.lower())))
-    
     out_file = f'riser_{get_datetime_now()}.csv'
     with open(out_file, 'a') as f: # TODO: Refactor, nested code ugly
-        while client.is_running:
+        while client.is_running():
             # Iterate through current batch of reads retrieved from client
             start_t = time.time()
             unblock_batch_reads = []
             stop_receiving_reads = []
-            for (channel, read) in client.get_read_chunks(batch_size=batch_size,
-                                                        last=True):
+            for (channel, read) in client.get_read_chunks():
                 # Preprocess raw signal if it's long enough
-                signal = np.frombuffer(read.raw_data, client.signal_dtype)
-                if len(signal) < processor.get_required_length():
+                signal = client.get_raw_signal(read)
+                if len(signal) < processor.get_required_length(): # TODO: Rename get_min_length
                     continue
                 signal = processor.process(signal)
 
@@ -79,10 +68,8 @@ def analysis(client, model, processor, target, logger, duration=0.1, throttle=4.
                 stop_receiving_reads.append((channel, read.number))
 
             # Send reject requests
-            if len(unblock_batch_reads) > 0: # TODO: What happens if this is omitted?
-                client.unblock_read_batch(unblock_batch_reads, duration=duration)
-            if len(stop_receiving_reads) > 0:
-                client.stop_receiving_batch(stop_receiving_reads)
+            client.reject_reads(unblock_batch_reads, duration)
+            client.track_assessed_reads(stop_receiving_reads)
 
             # Limit request rate
             end_t = time.time()
@@ -93,22 +80,10 @@ def analysis(client, model, processor, target, logger, duration=0.1, throttle=4.
                         len(unblock_batch_reads),
                         end_t - start_t)
         else:
-            send_message_to_minknow(client,
-                                    Severity.WARNING,
-                                    f'RISER has stopped running.')
+            client.send_message_to_minknow(Severity.WARNING,
+                                           f'RISER has stopped running.')
             logger.info("ReadUntil client stopped.")
 
-
-def setup_client(logger):
-    client = ReadUntilClient(filter_strands=True, #TODO: Is this needed?
-                             one_chunk=False,
-                             cache_type=AccumulatingCache)
-    client.run(first_channel=1, last_channel=512)
-    while client.is_running is False:
-        time.sleep(0.1)
-        logger.info('Waiting for client to start streaming live reads.')
-    logger.info('Client is running.')
-    return client
 
 
 def get_datetime_now():
@@ -146,9 +121,9 @@ def main():
 
     # Set up
     logger = setup_logging()
-    client = setup_client(logger)
+    client = Client(logger)
     config = get_config(config_file)
-    model = Model(model_file, config)
+    model = Model(model_file, config, logger)
     processor = SignalProcessor(polyA_length, input_length)
 
     # Log initial setup
@@ -164,6 +139,7 @@ def main():
     # function directly.
     # with ThreadPoolExecutor() as executor:
     #     executor.submit(analysis, read_until_client)
+    client.start_streaming_reads()
     analysis(client, model, processor, target, logger)
 
     # Close read stream
