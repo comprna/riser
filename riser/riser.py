d65001c41c624258aa168e18bad96819c60ecc5c
Alex Sneddon <asneddon.dev@gmail.com>
2023-07-24 13:29:00 +1000

    Fix typo CL arg parsing.

--- riser/riser.py
+++ riser/riser.py
@@ -57,18 +57,18 @@ def parse_args(parser):
             args.trim_length = 6474
 
     # If no config specified as arg, set based on target
-    if args.config is None:
+    if args.config_file is None:
         if args.target == 'mRNA':
-            args.config = 'model/mRNA_model.yaml'
+            args.config_file = 'model/mRNA_model.yaml'
         elif args.target == 'globin':
-            args.config = 'model/globin_model.yaml'
+            args.config_file = 'model/globin_model.yaml'
 
     # If no model specified as arg, set based on target
-    if args.model is None:
+    if args.model_file is None:
         if args.target == 'mRNA':
-            args.model = 'model/mRNA_model.pth'
+            args.model_file = 'model/mRNA_model.pth'
         elif args.target == 'globin':
-            args.model = 'model/globin_model.pth'
+            args.model_file = 'model/globin_model.pth'
 
     return args
 
